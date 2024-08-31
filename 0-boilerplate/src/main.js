199f098ecc76fc51d9b86d6ea4cf9051b9a35ebd
gayoung lee <ken604@naver.com>
2024-09-01 04:16:49 +0900

    큐브 완성, 보일러플레이트 시작!

--- 0-boilerplate/src/main.js
+++ 0-boilerplate/src/main.js
@@ -5,5 +5,38 @@ window.addEventListener("load", function () {
 });
 
 function init() {
-  console.log(Three);
+  const renderer = new Three.WebGLRenderer({
+    antialias: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new Three.Scene();
+
+  const camera = new Three.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+
+  camera.position.z = 5;
+
+  render();
+
+  function render() {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", handleResize);
 }
--- 1-cube/src/main.js
+++ 1-cube/src/main.js
@@ -1,11 +1,15 @@
 import * as Three from "three";
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
 
 window.addEventListener("load", function () {
   init();
 });
 
 function init() {
+  const optoins = {
+    color: 0x00ffff,
+  };
   const renderer = new Three.WebGLRenderer({
     antialias: true,
   });
@@ -21,11 +25,17 @@ function init() {
     1,
     500
   );
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  controls.autoRotate = true; //카메라의 자동 회전
+  // controls.autoRotateSpeed = 30; //회전 속도 조정
+  controls.enableDamping = true; //댐핑 효과를 활성화하는 설정
+  controls.dampingFactor = 0.01;
 
   const cubeGeometry = new Three.IcosahedronGeometry(1);
   const cubeMaterial = new Three.MeshLambertMaterial({
-    color: "aqua",
-    emissive: "#ffdcdc",
+    color: "#fcdcdc",
+
   });
   const cube = new Three.Mesh(cubeGeometry, cubeMaterial);
 
@@ -55,13 +65,16 @@ function init() {
   function render() {
     const elapsedTime = clock.getElapsedTime();
 
-    cube.rotation.x = elapsedTime;
-    cube.rotation.y = elapsedTime;
+    // cube.rotation.x = elapsedTime;
+    // cube.rotation.y = elapsedTime;
 
-    skeleton.rotation.x = elapsedTime * 2;
-    skeleton.rotation.y = elapsedTime * 2;
+    // skeleton.rotation.x = elapsedTime * 2;
+    // skeleton.rotation.y = elapsedTime * 2;
 
     renderer.render(scene, camera);
+
+    controls.update();
+
     requestAnimationFrame(render);
   }
 
@@ -70,7 +83,16 @@ function init() {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.render(scene, camera);
+    controls.update();
   }
 
   window.addEventListener("resize", handleResize);
+
+  const gui = new GUI();
+
+  gui.add(cube.position, "y", -3, 3);
+  gui.add(cube, "visible");
+  gui.addColor(optoins, "color").onChange((value) => {
+    cube.material.color.set(value);
+  });
 }
